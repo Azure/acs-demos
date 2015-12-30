72d4753b33fcfa19d7fdf7741a27cf14d6f84c84
Ross Gardler <ross@gardler.org>
2015-12-30 02:17:16 -0800

    add SMTP settings

--- src/config.py
+++ src/config.py
@@ -3,3 +3,10 @@ PROCESSED_LOG_FILE='/logs/archive.log'
 SUMMARY_LOG_FILE='/logs/summary.log'
 
 SLACK_WEBHOOK='https://hooks.slack.com/services/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'
+
+SMTP_SERVER=
+SMTP_PORT=
+SMTP_USERNAME=
+SMTP_PASSWORD=
+MAIL_FROM=
+MAIL_TO=
