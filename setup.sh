598d79d083c21a6d0732d6e3fc6b3087662ac406
Ross Gardler <ross@gardler.org>
2015-12-30 17:04:25 -0800

    ensure the containers are build correctly

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+docker build -t rgardler/acs-logging-test-base .
+docker build -t rgardler/acs-logging-test-simulate simulated_logging
+docker build -t rgardler/acs-logging-test-analyze analyze_logs
+docker build -t rgardler/acs-logging-test-cli cli
+
 docker create -v /logs --name base rgardler/acs-logging-test-base /bin/true
 docker run --volumes-from base --name simulate rgardler/acs-logging-test-simulate
 docker run --volumes-from base --name analyze rgardler/acs-logging-test-analyze
