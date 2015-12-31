4b01506796d1028487d4848a3b0d367cc8ca827d
Ross Gardler <ross@gardler.org>
2015-12-30 17:23:03 -0800

    don't name the setup containers, no need for them and they create a name clash if running a second or more time

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ docker build -t rgardler/acs-logging-test-analyze analyze_logs
 docker build -t rgardler/acs-logging-test-cli cli
 
 docker create -v /logs --name base rgardler/acs-logging-test-base /bin/true
-docker run --volumes-from base --name simulate rgardler/acs-logging-test-simulate
-docker run --volumes-from base --name analyze rgardler/acs-logging-test-analyze
-docker run --volumes-from base --name cli rgardler/acs-logging-test-cli
+docker run --volumes-from base rgardler/acs-logging-test-simulate
+docker run --volumes-from base rgardler/acs-logging-test-analyze
+docker run --volumes-from base rgardler/acs-logging-test-cli
