214a4c68d98a770f5bd9a7d7a7b68c3abd145741
Ross Gardler <ross@gardler.org>
2016-03-09 08:21:31 -0800

    make the test output more reabable

--- producer/test.sh
+++ producer/test.sh
@@ -1,24 +1,37 @@
 echo "#### building producer"
+echo
 docker build -t rgardler/acs-logging-test-producer:test .
 
+echo
 echo "#### running producer"
+echo
 id=$(docker run -d -p 5000:5000 --env-file ../env.conf rgardler/acs-logging-test-producer:test)
 sleep 2
 docker ps -l
 
+echo
 echo "#### check it's alive"
+echo
 curl http://localhost:5000/
 
-
+echo
 echo "#### enqueue a message"
+echo
 echo "BEFORE length"
+echo
 docker run --env-file ../env.conf rgardler/acs-logging-test-cli length
-curl -X PUT -d queue=acsdemo -d message=messagetext http://localhost:5000/enqueue
+curl -X POST -d queue=acsdemo -d message=messagetext http://localhost:5000/enqueue
+echo
 echo "AFTER length"
+echo
 docker run --env-file ../env.conf rgardler/acs-logging-test-cli length
 
+echo
 echo "#### stop producer"
+echo
 docker stop $id
 
+echo
 echo "####  producer logs"
+echo
 docker logs $id
