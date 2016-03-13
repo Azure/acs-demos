33eb2428fc92339195cd3d1afc350a4ea6f0576e
Ross Gardler <ross@gardler.org>
2016-03-12 16:54:51 -0800

    rename enqueue to fit naming conventions and add it to the build ad publish scripts.

--- publish.sh
+++ publish.sh
@@ -2,5 +2,6 @@ setup.sh
 
 docker push rgardler/acs-logging-test-base
 docker push rgardler/acs-logging-test-simulate
+docker push rgardler/acs-logging-test-rest-enqueue
 docker push rgardler/acs-logging-test-analyze
 docker push rgardler/acs-logging-test-cli
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker build -t rgardler/acs-logging-test-base .
 docker build -t rgardler/acs-logging-test-simulate simulated_logging
+docker build -t rgardler/acs-logging-test-rest-enqueue rest-enqueue
 docker build -t rgardler/acs-logging-test-analyze analyze_logs
 docker build -t rgardler/acs-logging-test-cli cli
 
