939417a8a82f0c1cb25bf52b171be2f7a3354510
Ross Gardler <ross@gardler.org>
2016-09-05 07:51:57 +0000

    WIP: basic 'scale need' endpoint, used to show how much in need of scaling up/down the analyzer is

--- analyze_logs/src/analyzer.py
+++ analyze_logs/src/analyzer.py
@@ -15,6 +15,7 @@ import os
 import re
 import socket
 import sys
+import threading
 import time
 import traceback
 
@@ -22,16 +23,16 @@ global summary
 
 class Analyzer:
 
+  time_since_last_event = 10
+  last_event_time = time.time()
+  
   def __init__(self):
     self.log = Log()
     self.log.debug("Storage account for analyzer: {0}".format(config.AZURE_STORAGE_ACCOUNT_NAME))
     self.msgQueue = Queue(account_name = config.AZURE_STORAGE_ACCOUNT_NAME, account_key=config.AZURE_STORAGE_ACCOUNT_KEY, queue_name=config.AZURE_STORAGE_QUEUE_NAME)
     self.summary = SummaryTable(config.AZURE_STORAGE_ACCOUNT_NAME, config.AZURE_STORAGE_ACCOUNT_KEY, config.AZURE_STORAGE_SUMMARY_TABLE_NAME)
-    self.keep_running = bool(config.ANALYZER_KEEP_RUNNING)
-    self.log.debug("Analyzer keep running: {0}".format(self.keep_running))
-    if self.keep_running:
-      self.sleep_time = float(config.ANALYZER_SLEEP_TIME)
-      self.log.debug("Sleep time between analyses: {0}".format(self.sleep_time))
+    self.sleep_time = float(config.ANALYZER_SLEEP_TIME)
+    self.log.debug("Sleep time between analyses: {0}".format(self.sleep_time))
 
   def incrementCount(self, event_type):
     count = self.summary.getCount(event_type)
@@ -60,6 +61,9 @@ class Analyzer:
     while True:
       events = self.msgQueue.dequeue()
       if len(events) > 0:
+        now = time.time()
+        self.time_since_last_event = now - self.last_event_time
+        self.last_event_time = now
         for event in events:
           self.log.info("Dequeued: " + event.message_text)
           try:
@@ -70,26 +74,34 @@ class Analyzer:
             e = sys.exc_info()[0]
             self.log.error("Could not process: " + event.message_text + " because %s" % e)
             traceback.print_exc(file=sys.stdout)
-      else:
-          break
       time.sleep(self.sleep_time)   
 
 
-app =Flask(__name__)
-
+app = Flask(__name__)
+analyzer = Analyzer()
+  
 @app.route("/")
-def scale():
-  response = {
-      "current": 0
-      }
+def scale_need():
+  """Returns a value between -100 and 100. -100 indicates no work, scale
+  down immediately. +100 indicates, far too much work, scale up
+  immediately. 0 means, just right, don't change anything. Values in
+  betwwen indicate a scale of confidence the application has in coping
+  with current load.
+  """
+
+  if (analyzer.time_since_last_event < 2):
+    status = 100
+  elif (analyzer.time_since_last_event > 5):
+    status = -100
+  else:
+    status = 0
+
+  response = { "status": status }
 
   return jsonify(response)
-  
+
 if __name__ == "__main__":
   app.debug = True
-  app.run(host='0.0.0.0', port = 5555)
+  app.run(host='0.0.0.0')
 
-  analyzer = Analyzer()
-  analyzer.fullAnalysis()
-
-  
+  threading.Thread(target=analyzer.fullAnalysis).start()
