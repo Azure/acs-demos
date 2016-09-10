d32f68de59d9e475e175475e9343981bf5812cf5
Ross Gardler <ross@gardler.org>
2016-09-10 19:18:30 +0000

    add the test-query to the publish and setup scripts

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ docker build -t rgardler/acs-logging-test-base .
 docker build -t rgardler/acs-logging-test-simulate simulated_logging
 docker build -t rgardler/acs-logging-test-web web
 docker build -t rgardler/acs-logging-test-rest-enqueue rest-enqueue
+docker build -t rgardler/acs-logging-test-rest-query rest-query
 docker build -t rgardler/acs-logging-test-analyze analyze_logs
 docker build -t rgardler/acs-logging-test-cli cli
 docker build -t rgardler/acs-logging-test-microscaling microscaling
