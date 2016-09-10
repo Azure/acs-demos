e4b0fdff677e0ad37647bb0ad7ddd132ee2bf896
Ross Gardler <ross@gardler.org>
2016-09-10 19:17:52 +0000

    Since the queue_id param is passed in we cannot initialize the queue object at instantiation, otherwise it would always use the same queue_id

--- rest-query/src/server.py
+++ rest-query/src/server.py
@@ -8,15 +8,19 @@ app = Flask(__name__)
 api = Api(app)
 
 class QueueAPI(Resource):
-    queue_id = "acstest"
-    def __init__(self):
-        self.queue = Queue(account_name = config.AZURE_STORAGE_ACCOUNT_NAME, account_key=config.AZURE_STORAGE_ACCOUNT_KEY, queue_name=self.queue_id)
 
     @app.route("/")
     def hello():
         return render_template('index.html')
 
     def get(self, queue_id):
+        """Get details of a queue. 
+        """
+        
+        if not queue_id:
+            queue_id = config.AZURE_STORAGE_QUEUE_NAME
+
+        queue = self.getMessageQueue(queue_id)
         length = self.queue.getLength()
         resp = {
             'queue_name': queue_id,
@@ -24,7 +28,11 @@ class QueueAPI(Resource):
             }
         return resp
 
-api.add_resource(QueueAPI, '/queue/<string:queue_id>')
+    def getMessageQueue(self, queue_id):
+        self.queue = Queue(account_name = config.AZURE_STORAGE_ACCOUNT_NAME, account_key=config.AZURE_STORAGE_ACCOUNT_KEY, queue_name=queue_id)
+    
+api.add_resource(QueueAPI, '/queue',
+                           '/queue/<string:queue_id>')
 
 if __name__ == "__main__":
     app.debug = True
