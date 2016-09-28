c992607008318d6f3e62347ba79af5bd882407d6
Ross Gardler <ross@gardler.org>
2016-09-28 19:21:34 +0000

    if rest api is not responding we should ignore the error and carry on, the orchestrator will fix it eventually

--- web/src/js/queueData.js
+++ web/src/js/queueData.js
@@ -17,53 +17,63 @@ function plotQueue(queueId) {
 	d = new Date();
 	now = d.getTime()
         queueEndpoint = "http://" + window.location.hostname + ":5555/queue/" + queueId + "?time=" + now;
-        $.ajax({url: queueEndpoint, success: function(result){
-            $("#queue_length").text(result.queue_length);
-	    duration = parseFloat(result.last_duration);
-	    duration = Math.round(duration) / 1000;
-	    $("#processing_time").text(duration);
-	    if (typeof length_dataX == 'undefined') {
-		length_dataX = [0];
-		length_dataY = [0];
-		time_dataX = [0];
-		time_dataY = [0];
-	    } else {
-		x = length_dataX[length_dataX.length - 1] + 1;
-		length_dataX.push(x);
-		length_dataY.push(result.queue_length);
-		time_dataX.push(x);
-		time_dataY.push(duration);
-	    }
-	    if (length_dataX.length > 60) {
-		length_dataX = length_dataX.splice(1, 59);
-		length_dataY = length_dataY.splice(1, 59);
-		time_dataX = time_dataX.splice(1, 59);
-		time_dataY = time_dataY.splice(1, 59);
-	    }
-						   
-	    length = {
-		x: length_dataX,
-		y: length_dataY,
-		type: 'scatter',
-		name: 'Queue Length'
-	    };
-	    time = {
-		x: time_dataX,
-		y: time_dataY,
-		type: 'scatter',
-		name: 'Processing Time'
-	    };
-	    var data = [length, time];
 
-	    var layout = {
-		showlegend: false
-	    };
-	    
-	    Plotly.newPlot('lengthChart', data, layout);
-	    setTimeout(function() {
-		updateLength(queueId);
-	    }, 1000);
-        }});
+        $.ajax({url: queueEndpoint,
+		contentType:"application/json; charset=utf-8",
+		success: function(result){
+		    $("#queue_length").text(result.queue_length);
+		    duration = parseFloat(result.last_duration);
+		    duration = Math.round(duration) / 1000;
+		    $("#processing_time").text(duration);
+		    if (typeof length_dataX == 'undefined') {
+			length_dataX = [0];
+			length_dataY = [0];
+			time_dataX = [0];
+			time_dataY = [0];
+		    } else {
+			x = length_dataX[length_dataX.length - 1] + 1;
+			length_dataX.push(x);
+			length_dataY.push(result.queue_length);
+			time_dataX.push(x);
+			time_dataY.push(duration);
+		    }
+		    if (length_dataX.length > 60) {
+			length_dataX = length_dataX.splice(1, 59);
+			length_dataY = length_dataY.splice(1, 59);
+			time_dataX = time_dataX.splice(1, 59);
+			time_dataY = time_dataY.splice(1, 59);
+		    }
+		    
+		    length = {
+			x: length_dataX,
+			y: length_dataY,
+			type: 'scatter',
+			name: 'Queue Length'
+		    };
+		    time = {
+			x: time_dataX,
+			y: time_dataY,
+			type: 'scatter',
+			name: 'Processing Time'
+		    };
+		    var data = [length, time];
+
+		    var layout = {
+			showlegend: false
+		    };
+		    
+		    Plotly.newPlot('lengthChart', data, layout);
+		    setTimeout(function() {
+			updateLength(queueId);
+		    }, 1000);
+		},
+		error: function (request, status, error) {
+		    // Ignore REST API errors, the orchestrartor will bring it back up in time		    
+		    setTimeout(function() {
+			updateLength(queueId);
+		    }, 1000);
+		}
+	       });
     }
 
     updateLength();
