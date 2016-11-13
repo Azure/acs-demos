5c9d3f572a8145281a1fbfd9db5add3c1838f34b
Ross Gardler <ross@gardler.org>
2016-11-12 22:27:06 -0800

    add very early code for olympics card game

---
+++ olympics/src/main/java/org/gardler/biglittlechallenge/olympics/model/Deck.java
@@ -0,0 +1,41 @@
+package org.gardler.biglittlechallenge.olympics.model;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Deck {
+	String name;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	HashMap<String, Card> cards = new HashMap<String, Card>();
+	
+	public Deck(String name) {
+		this.setName(name);
+	}
+
+	public HashMap<String, Card> getCards() {
+		return cards;
+	}
+	
+	public void addCard(Card card) {
+		cards.put(card.getName(), card);
+	}
+	
+	public String toString() {
+		String result = "";
+		Iterator<Card> itr = getCards().values().iterator();
+		while (itr.hasNext()) {
+			Card card = itr.next();
+			result = result + "\n" + card.toString();
+		}
+		return result;
+	}
+
+}
