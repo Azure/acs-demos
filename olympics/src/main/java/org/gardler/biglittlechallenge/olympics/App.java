61636066e09eb628a1630611c2ae4f51458e6bcd
Ross Gardler <ross@gardler.org>
2016-11-25 13:19:05 -0800

    Add a basic AI so that we can test the properties of cards

--- olympics/src/main/java/org/gardler/biglittlechallenge/olympics/App.java
+++ olympics/src/main/java/org/gardler/biglittlechallenge/olympics/App.java
@@ -1,8 +1,6 @@
 package org.gardler.biglittlechallenge.olympics;
 
-import org.gardler.biglittlechallenge.olympics.model.Deck;
-import org.gardler.biglittlechallenge.olympics.model.Player;
-import org.gardler.biglittlechallenge.olympics.model.Character;
+import org.gardler.biglittlechallenge.olympics.ai.DumbPlayer;
 
 /**
  * Hello world!
@@ -12,24 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Player player1 = new Player("Player One");
-    	
-    	Deck deck = new Deck("Player 1 First Deck");
-    	Character card = new Character("Foo");
-    	deck.addCard(card);
-    	card = new Character("Potato");
-    	deck.addCard(card);
-    	player1.setDeck(deck);
-    	
-    	Player player2 = new Player("Player Two");
-    	deck = new Deck("Player 2 First Deck");
-    	card = new Character("Foo");
-    	deck.addCard(card);
-    	card = new Character("Pie");
-    	deck.addCard(card);
-    	player2.setDeck(deck);
-    	
-        System.out.println( "Hello from " + player1.getName() );
+    	DumbPlayer player1 = new DumbPlayer("AI Player One");
+    	DumbPlayer player2 = new DumbPlayer("AI Player Two");
+
+    	System.out.println( "Hello from " + player1.getName() );
         System.out.println( player1.getDeck().getName());
         System.out.println(player1.getDeck().toString());
         
