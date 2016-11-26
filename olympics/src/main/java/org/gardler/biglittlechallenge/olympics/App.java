3d122552b840e7406434a0bf1fb283bd02d51ddf
Ross Gardler <ross@gardler.org>
2016-11-25 19:29:20 -0800

    add some very basic rules for the 100m sprint

--- olympics/src/main/java/org/gardler/biglittlechallenge/olympics/App.java
+++ olympics/src/main/java/org/gardler/biglittlechallenge/olympics/App.java
@@ -1,8 +1,10 @@
 package org.gardler.biglittlechallenge.olympics;
 
-import org.gardler.biglittlechallenge.core.model.Card;
-import org.gardler.biglittlechallenge.olympics.model.Character;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.gardler.biglittlechallenge.olympics.ai.DumbPlayer;
+import org.gardler.biglittlechallenge.olympics.model.Character;
 import org.gardler.biglittlechallenge.olympics.model.Player;
 
 /**
@@ -15,34 +17,45 @@ public class App
     {
     	DumbPlayer player1 = new DumbPlayer("AI Player One");
     	DumbPlayer player2 = new DumbPlayer("AI Player Two");
+    	DumbPlayer player3 = new DumbPlayer("AI Player Three");
+    	DumbPlayer player4 = new DumbPlayer("AI Player Four");
+    	DumbPlayer player5 = new DumbPlayer("AI Player Five");
 
-    	Character player1card = player1.playCharacter();
-    	Character player2card = player2.playCharacter();
-    	
-    	// Play 100M Sprint
-    	int player1Rating = player1card.getSpeed() + player1card.getReactions();
-    	int player2Rating = player2card.getSpeed() + player2card.getReactions();
-    	
-    	Player winner;
-    	if (player1Rating > player2Rating) {
-    		winner = player1;
-    	} else if (player1Rating < player2Rating) {
-    		winner = player2;
-    	} else {
-    		winner = null;
+    	Player[] players = { player1, player2, player3, player4, player5 };
+    	plau100MSprint(players);
+    }
+
+    /**
+     * Play the 100M sprint with the supplied players.
+     * 
+     * @param players
+     */
+	private static void plau100MSprint(Player[] players) {
+		List <Character> characters = new ArrayList<Character>();
+		List <Integer> ratings = new ArrayList<Integer>();
+		
+		int numOfPlayers = players.length;
+		for (int i = 0; i < numOfPlayers; i++) {
+			System.out.println(players[i]);
+			Character character = players[i].playCharacter();
+			characters.add(character);
+			Integer rating = character.getSpeed() + character.getReactions();
+			ratings.add(rating);
+		}
+		    	
+    	Player winner = null;
+    	Integer highestRating = 0;
+    	for (int i = 0; i < numOfPlayers; i++ ) {
+    		if (highestRating < ratings.get(i)) {
+    			highestRating = ratings.get(i);
+    			winner = players[i];
+    		}
     	}
 
-    	System.out.println(player1.toString());
-    	System.out.println("Player 1 rating for 100m Sprint = " + player1Rating);
-
-    	System.out.println(player2.toString());
-    	System.out.println("Player 2 rating for 100m Spring = " + player2Rating);
-    	System.out.println("");
-    	
     	if (winner != null) {
     		System.out.println("Winner of 100m sprint is " + winner.getName());
     	} else {
     		System.out.println("100m sprint was a draw");
     	}
-    }
+	}
 }
