a3f6a33efc414433807054738f16e618391e4d04
Ross Gardler <ross@gardler.org>
2016-12-22 12:23:42 -0800

    move full game test into the tournament object

--- trials-engine/src/test/java/org/gardler/biglittlechallenge/trials_engine/TournamentAPITest.java
+++ trials-engine/src/test/java/org/gardler/biglittlechallenge/trials_engine/TournamentAPITest.java
@@ -4,26 +4,9 @@ import static org.junit.Assert.*;
 
 import org.gardler.biglittlechallenge.core.model.GameStatus;
 import org.gardler.biglittlechallenge.trials.engine.TournamentAPI;
-import org.gardler.biglittlechallenge.trials.model.Player;
 import org.junit.Test;
 
 public class TournamentAPITest {
-
-	@Test
-	public void testFullGame() {
-		String uiClassName = "org.gardler.biglittlechallenge.trials.ai.DumbAIUI";
-		Player player = new Player("Test AI Player", uiClassName); 
-		
-		TournamentAPI api = initTournament();
-		GameStatus status = api.postJoinGame(player);
-		assertEquals("Game status is not 'WaitingForPlayers'", GameStatus.State.WaitingForPlayers, status.getState());
-		
-		player = new Player("Second Test AI Player", uiClassName);
-		status = api.postJoinGame(player);
-		assertEquals("Game status is not 'Playing' after second player joins", GameStatus.State.Playing, status.getState());
-		
-		// Verify engine waits for players to call back
-	}
 
 	@Test
 	public void testGetStatus() {
@@ -34,7 +17,7 @@ public class TournamentAPITest {
 
 	private TournamentAPI initTournament() {
 		TournamentAPI api = new TournamentAPI();
-		api.abortGame();
+		api.abortGame(); // This is to ensure we are not using a previously created game
 		return api;
 	}
 
--- trials-engine/src/test/java/org/gardler/biglittlechallenge/trials_engine/TournamentTest.java
+++ trials-engine/src/test/java/org/gardler/biglittlechallenge/trials_engine/TournamentTest.java
@@ -1,7 +1,13 @@
 package org.gardler.biglittlechallenge.trials_engine;
 
+import static org.junit.Assert.assertEquals;
+
 import org.gardler.biglittlechallenge.core.model.AbstractGame;
+import org.gardler.biglittlechallenge.core.model.GameStatus;
+import org.gardler.biglittlechallenge.trials.EngineControllerApp;
 import org.gardler.biglittlechallenge.trials.engine.Tournament;
+import org.gardler.biglittlechallenge.trials.engine.TournamentAPI;
+import org.gardler.biglittlechallenge.trials.model.Player;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,5 +24,22 @@ public class TournamentTest extends TestCase {
 		logger.info("Tournament toString is: " + result);
 		assertTrue("We don;t appear to have set the rounds in the tournament correctly", result.contains("consists of 4 events"));
 	}
-
+	
+	public void testFullGame() {
+		String uiClassName = "org.gardler.biglittlechallenge.trials.ai.DumbAIUI";
+		Player player = new Player("Test AI Player", uiClassName); 
+		
+		TournamentAPI api = new TournamentAPI();
+		api.abortGame(); // This is to ensure we are not using a previously created game
+		
+		GameStatus status = api.postJoinGame(player);
+		assertEquals("Game status is not 'WaitingForPlayers'", GameStatus.State.WaitingForPlayers, status.getState());
+		
+		player = new Player("Second Test AI Player", uiClassName);
+		status = api.postJoinGame(player);
+		assertEquals("Game status is not 'Playing' after second player joins", GameStatus.State.Playing, status.getState());
+		
+		
+		
+	}
 }
