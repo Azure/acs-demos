bb7a02dcc109df6a4fff7c51a1a41291bb601b31
Ross Gardler <rgardler@apache.org>
2017-01-03 07:59:55 +0000

    build files for ARM

---
+++ build-arm.sh
@@ -0,0 +1,5 @@
+docker build -t biglittlechallenge/trials-base-arm -f Dockerfile-trials-base-arm .
+
+docker build -t biglittlechallenge/trials-engine-arm -f Dockerfile-trials-engine-arm .
+
+docker build -t biglittlechallenge/trials-ai-arm -f Dockerfile-trials-ai-arm .
