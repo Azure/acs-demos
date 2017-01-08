00e8656c796aa8e658552e307e3bb693208d85bd
Ross Gardler <ross@gardler.org>
2017-01-08 13:06:45 -0800

    make the Engine Dashboard update every second

--- trials-engine-dashboard/src/js/engine.js
+++ trials-engine-dashboard/src/js/engine.js
@@ -1,15 +1,25 @@
-function getStatus(queueId) {
-    d = new Date();
-    now = d.getTime()
-    endpoint = "http://localhost:8080/api/v0.1/tournament/status";
+function monitorStatus() {
+    function updateStatus() {
+	d = new Date();
+	now = d.getTime()
+	endpoint = "http://localhost:8080/api/v0.1/tournament/status";
 
-    $.ajax({url: endpoint,
-	    contentType:"application/json; charset=utf-8",
-	    success: function(result){
-		        $("#state").text(result.state);
-	    },
-	    error: function (request, status, error) {
-		// Ignore REST API errors, the orchestrartor will bring it back up in time    
-	    }
-	   });
+	$.ajax({url: endpoint,
+		contentType:"application/json; charset=utf-8",
+		success: function(result){
+		    $("#state").text(result.state);
+		    setTimeout(function() {
+			updateStatus();
+		    }, 1000);
+		},
+		error: function (request, status, error) {
+		    // Ignore REST API errors, the orchestrartor will bring it back up in time    
+		    setTimeout(function() {
+			updateStatus();
+		    }, 1000);
+		}
+	       });
+    }
+
+    updateStatus();
 }
