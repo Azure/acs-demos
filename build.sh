d0860d44ce372bd08c258ee075d8ddfcd5620981
Ross Gardler <ross@gardler.org>
2017-01-12 00:04:57 -0800

    add build for Engine Dashboard

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@ docker build -t biglittlechallenge/trials-base -f Dockerfile-trials-base .
 
 docker build -t biglittlechallenge/trials-engine -f Dockerfile-trials-engine .
 
+docker build -t biglittlechallenge/trials-engine-dashboard -f Dockerfile-trials-engine-dashboard .
+
 docker build -t biglittlechallenge/trials-ai -f Dockerfile-trials-ai .
