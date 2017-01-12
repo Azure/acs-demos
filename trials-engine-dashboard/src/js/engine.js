266f5132a1f3946feff577f77df662631bbc3a32
Ross Gardler <ross@gardler.org>
2017-01-11 20:11:07 -0800

    Add a very basic engine dashboard using websockets. Listens on port 8181

--- trials-engine-dashboard/src/js/engine.js
+++ trials-engine-dashboard/src/js/engine.js
@@ -1,25 +1,34 @@
-function monitorStatus() {
-    function updateStatus() {
-	d = new Date();
-	now = d.getTime()
-	endpoint = "http://localhost:8080/api/v0.1/tournament/status";
+function monitorStatusSocket() {
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/engine');
 
-	$.ajax({url: endpoint,
-		contentType:"application/json; charset=utf-8",
-		success: function(result){
-		    $("#state").text(result.state);
-		    setTimeout(function() {
-			updateStatus();
-		    }, 1000);
-		},
-		error: function (request, status, error) {
-		    // Ignore REST API errors, the orchestrartor will bring it back up in time    
-		    setTimeout(function() {
-			updateStatus();
-		    }, 1000);
-		}
-	       });
-    }
+    socket.on('game_status', function(msg) {
+	status = $.parseJSON(msg)
 
-    updateStatus();
+        $('#log').prepend('<br/>' + $('<div/>').text(new Date() + " : " + JSON.stringify(status)).html());
+        $("#state").text(status.state);
+
+	var players = [];
+        $.each(status.playerResults, function(index, player) {
+            players.push('<li>' + player.name + ' (' + player.points + ' points)</li>')
+	});
+	$("#player-table").empty();
+	$("#player-table").append(players.join(''));
+    });
+
+    var ping_pong_times = [];
+    var start_time;
+    window.setInterval(function() {
+      start_time = (new Date).getTime();
+      socket.emit('engine_ping');
+    }, 1000);
+
+    socket.on('engine_pong', function() {
+      var latency = (new Date).getTime() - start_time;
+      ping_pong_times.push(latency);
+      ping_pong_times = ping_pong_times.slice(-30);
+      var sum = 0;
+      for (var i = 0; i < ping_pong_times.length; i++)
+        sum += ping_pong_times[i];
+        $('#ping-pong').text(Math.round(10 * sum / ping_pong_times.length) / 10);
+      });
 }
--- trials-engine-dashboard/src/server.py
+++ trials-engine-dashboard/src/server.py
@@ -2,19 +2,56 @@
 A simple web dashboard for examining the state of a game engine.
 
 """
-from flask import Flask, jsonify, redirect, request, render_template, url_for, send_from_directory
+from flask import Flask, jsonify, redirect, request, render_template, url_for, send_from_directory, json
+from flask.ext.socketio import SocketIO, emit
+import json
+import urllib
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+engine_url = "http://engine:8080/api/v0.1/tournament/status"
+
+def background_thread():
+    old_data = None;
+    while True:
+        socketio.sleep(1)
+        resp_body = urllib.request.urlopen(engine_url).read()
+        status = resp_body.decode('UTF-8')
+
+        socketio.emit('game_status',
+                      status,
+                      namespace='/engine')
+
 @app.route("/")
 def index(name=None):
-    status = { 'state': 'Fetching...'}
+    status = { 'state': 'Initializing Dashboard...'}
     return render_template('index.html',
-                           status = status
+                           status = status,
+                           async_mode=socketio.async_mode
     )
+
+@socketio.on('connect', namespace='/engine')
+def test_connect():
+    global thread
+    if thread is None:
+        thread = socketio.start_background_task(target=background_thread)
+    emit('log', 'Connected to engine')
+
+@socketio.on('engine_ping', namespace='/engine')
+def ping_pong():
+    emit('engine_pong')
+
 @app.route('/js/<path:filename>')
 def send_js(filename):
     return send_from_directory('js', filename)
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    socketio.run(app, host='0.0.0.0', port=80)
