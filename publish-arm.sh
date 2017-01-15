bf0a01d81e8f6671993efb80d12b66f903310281
Ross Gardler <rgardler@apache.org>
2017-01-15 13:06:24 -0800

    Add engine-dashboard to run and publish scripts

--- publish-arm.sh
+++ publish-arm.sh
@@ -2,4 +2,6 @@ docker push biglittlechallenge/trials-base-arm
 
 docker push biglittlechallenge/trials-engine-arm
 
-docker push -t biglittlechallenge/trials-ai-arm
+docker push biglittlechallenge/trials-engine-dashboard-arm
+
+docker push biglittlechallenge/trials-ai-arm
