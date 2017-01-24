cbc31233c203de38daff003f35e81d27eb9ca4f0
Ross Gardler <ross@gardler.org>
2017-01-23 23:00:29 -0800

    add dashbaord to publish (and incorrect switch)

--- publish.sh
+++ publish.sh
@@ -2,4 +2,6 @@ docker push biglittlechallenge/trials-base
 
 docker push biglittlechallenge/trials-engine
 
-docker push -t biglittlechallenge/trials-ai
+docker push biglittlechallenge/trials-engine-dashboard
+
+docker push biglittlechallenge/trials-ai
