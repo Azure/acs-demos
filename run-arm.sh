754adf74c26d42ae4731c91edf29575c7d738cf4
Ross Gardler <ross@gardler.org>
2017-02-06 01:57:37 -0800

    move pi scripts into a pi folder

--- run-arm.sh
+++
@@ -1,9 +0,0 @@
-docker network create --driver overlay --subnet 20.0.14.0/24 trials
-
-docker service create --replicas=1 --network=trials --publish 8080:8080 --name engine --env MIN_NUMBER_OF_PLAYERS=5 biglittlechallenge/trials-engine-arm
-
-docker service create --replicas=1 --network=trials --publish 8181:80 --name engine-dashboard biglittlechallenge/trials-engine-dashboard-arm
-
-docker service create --name aiplayer --replicas=2 --network=trials --publish 8888 biglittlechallenge/trials-ai-arm
-
-echo "use 'docker service scale aiplayer=5' to add more players"
