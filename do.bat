6f7468807c3a88a606fabe8bba47f29bf1659df4
Ross Gardler <ross@gardler.org>
2017-05-06 22:56:56 -0700

    remove someones experiment script that crept into a commit

--- do.bat
+++
@@ -1,7 +0,0 @@
-git init
-git add --all
-git remove do.bat
-git remove do.bat*
-git commit -m "first commit"
-git remote add origin https://github.com/brunoterkaly/acs-demos.git
-git push -u origin master
