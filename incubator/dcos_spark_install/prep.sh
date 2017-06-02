6a2d490bbb54d46ed1ab1b8980bc8ac8f329f852
Ross Gardler <ross@gardler.org>
2017-06-02 23:37:52 +0000

    use docker container

--- incubator/dcos_spark_install/prep.sh
+++ incubator/dcos_spark_install/prep.sh
@@ -1,8 +1,2 @@
-# sudo python3 ~/projects/simdem/run.py run --path ./ preparation
-
-pushd ~/projects/simdem/
-./build.sh
-popd
-
 docker rm simdem
 docker run -it --name simdem -v ~/.ssh:/root/.ssh -v ~/.azure:/root/.azure  -v $(pwd):/demo_scripts rgardler/simdem run preparation
