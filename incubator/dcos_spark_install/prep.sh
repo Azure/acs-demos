ef4273ef9afefdf66fc30ce0d8e934de112a047b
Ross Gardler <ross@gardler.org>
2017-06-02 23:36:28 +0000

    copy enviornment settings into the commadn process and remove use of a global that created side effects

--- incubator/dcos_spark_install/prep.sh
+++ incubator/dcos_spark_install/prep.sh
@@ -1,1 +1,8 @@
-sudo python3 ~/projects/simdem/run.py run --path ./ preparation
+# sudo python3 ~/projects/simdem/run.py run --path ./ preparation
+
+pushd ~/projects/simdem/
+./build.sh
+popd
+
+docker rm simdem
+docker run -it --name simdem -v ~/.ssh:/root/.ssh -v ~/.azure:/root/.azure  -v $(pwd):/demo_scripts rgardler/simdem run preparation
