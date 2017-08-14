12c39eff48dc2946aca58ceefbe63749fd745776
Ross Gardler <ross@gardler.org>
2017-08-14 05:20:24 +0000

    make use of the prereqiusites and validation. Add play_ai_only script. Improve reliabity.

--- incubator/BigLittleChallenge/kubernetes/cleanup.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete service engine
-kubectl delete service aiplayer
-kubectl delete service dashboard
-
-kubectl delete deployment engine
-kubectl delete deployment aiplayer
-kubectl delete deployment dashboard
